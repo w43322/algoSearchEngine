59e7221bcf0410dd0413db65efc5ba7c8eafcc45
w43322 <raywang777@foxmail.com>
2022-06-11 17:49:01 +0800

    fix absolute path

--- include/searchengine/searchengine.h
+++ include/searchengine/searchengine.h
@@ -7,11 +7,11 @@
 #include "../indexing/indexing.h"
 #include "../cppjieba/Jieba.hpp"
 
-const char* const DICT_PATH = "/Users/w43322/Desktop/code/algo/dict/jieba.dict.utf8";
-const char* const HMM_PATH = "/Users/w43322/Desktop/code/algo/dict/hmm_model.utf8";
-const char* const USER_DICT_PATH = "/Users/w43322/Desktop/code/algo/dict/user.dict.utf8";
-const char* const IDF_PATH = "/Users/w43322/Desktop/code/algo/dict/idf.utf8";
-const char* const STOP_WORD_PATH = "/Users/w43322/Desktop/code/algo/dict/stop_words.utf8";
+const char* const DICT_PATH = "./dict/jieba.dict.utf8";
+const char* const HMM_PATH = "./dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "./dict/user.dict.utf8";
+const char* const IDF_PATH = "./dict/idf.utf8";
+const char* const STOP_WORD_PATH = "./dict/stop_words.utf8";
 
 class SearchEngine
 {
